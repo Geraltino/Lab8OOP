6acd0483b6f66f11aa44b418c83419d75f71084d
Igor Blok <G00415662@atu.ie>
2023-11-21 14:05:52 +0000

    Added writer.append to the code, Works and allows more data to be added without removing previous data

--- src/main/java/ie/atu/fileWriter.java
+++ src/main/java/ie/atu/fileWriter.java
@@ -2,21 +2,28 @@ package ie.atu;
 import java.io.FileWriter;
 import java.io.IOException;
 public class fileWriter {
-
         public static void main(String[] args)
         {
+
             // initialize a string
             String fileName = "File13.txt";
             try(FileWriter writer = new FileWriter(fileName)) {
-                writer.write("Please stop typing.");
+
+                writer.write("Top Text");
+
+                writer.append("\nBottom text");
 
                 System.out.println("Content has successfully been written to the file");
+
             }
             catch (IOException e) {
                 System.out.println("Error");
                 e.printStackTrace();
             }
+
         }
-    }
+
+        }
+
 
 
