835660d34d042539fd5d4f4c486e82a2eecc0bf6
Igor Blok <G00415662@atu.ie>
2023-11-21 14:00:40 +0000

    Redid the File as lecturer has it done

--- src/main/java/ie/atu/fileWriter.java
+++ src/main/java/ie/atu/fileWriter.java
@@ -6,24 +6,15 @@ public class fileWriter {
         public static void main(String[] args)
         {
             // initialize a string
-            String fileWriter = "This is a new string to add to the file";
-            try {
+            String fileName = "File13.txt";
+            try(FileWriter writer = new FileWriter(fileName)) {
+                writer.write("Please stop typing.");
 
-                // attach a file to FileWriter
-                FileWriter fw = new FileWriter("C:\\Users\\G00415662@atu.ie\\OneDrive - Atlantic TU\\OOP\\Lab8\\Lab8OOP\\File13.txt");
-
-                // read each character from string and write
-                // into FileWriter
-                for (int i = 0; i < fileWriter.length(); i++)
-                    fw.write(fileWriter.charAt(i));
-
-                System.out.println("Successfully written");
-
-                // close the file
-                fw.close();
+                System.out.println("Content has successfully been written to the file");
             }
             catch (IOException e) {
-                e.getStackTrace();
+                System.out.println("Error");
+                e.printStackTrace();
             }
         }
     }
